b3d32db2c815ecbabcbfa219a1b9aa4f157c05a2
Lars Gerber <social@larsgerber.ch>
2024-05-28 10:47:18 +0200

    fix: add command to restart audio driver

--- dot_config/scripts/executable_appleScript.sh
+++ dot_config/scripts/executable_appleScript.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+defaults write NSGlobalDomain com.apple.sound.beep.flash -bool false
+defaults write com.apple.universalaccess flashScreen -bool false; sudo killall coreaudiod
 defaults write com.apple.Dock showhidden -boolean yes; killall Dock
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
