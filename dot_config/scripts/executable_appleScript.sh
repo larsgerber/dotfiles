3f293d6dbcdb6c33103a525bf3a4482c87dfb60f
Lars Gerber <social@larsgerber.ch>
2024-07-30 14:01:36 +0200

    feat: add command to prevent .DS_Store file creation

--- dot_config/scripts/executable_appleScript.sh
+++ dot_config/scripts/executable_appleScript.sh
@@ -4,4 +4,5 @@ defaults write NSGlobalDomain com.apple.sound.beep.flash -bool false
 defaults write com.apple.universalaccess flashScreen -bool false; sudo killall coreaudiod
 defaults write com.apple.Dock showhidden -boolean yes; killall Dock
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
