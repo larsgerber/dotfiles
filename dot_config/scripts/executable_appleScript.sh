95cc77fd597e18cf5649fc197beb4fd4c823c619
Lars Gerber <github@larsgerber.ch>
2024-08-19 19:59:07 +0200

    fix: add command to delay dock relocation

--- dot_config/scripts/executable_appleScript.sh
+++ dot_config/scripts/executable_appleScript.sh
@@ -3,6 +3,7 @@
 defaults write NSGlobalDomain com.apple.sound.beep.flash -bool false
 defaults write com.apple.universalaccess flashScreen -bool false; sudo killall coreaudiod
 defaults write com.apple.Dock showhidden -boolean yes; killall Dock
+defaults write com.apple.dock autohide-delay -float 1; killall Dock
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
