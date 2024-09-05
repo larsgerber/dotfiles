5116584b84db49e41623db8c2c4d82cb0725df43
Lars Gerber <github@larsgerber.ch>
2024-09-05 11:36:17 +0200

    chore: improve VSCodium config

--- dot_config/scripts/executable_teleport.sh
+++ dot_config/scripts/executable_teleport.sh
@@ -10,13 +10,19 @@ IFS=$'\n\t'
 apps=$(tsh apps ls --format yaml)
 
 # select a cluster from list, skip promt when only one match
-cluster=$(echo "$apps" | yq '.[].metadata.labels.cluster' | sort | uniq | fzf --select-1 --query="${1:-}" || true)
+cluster=$(
+  echo "$apps" | yq '.[].metadata.labels.cluster' | sort | uniq | fzf --select-1 --query="${1:-}" || true
+)
 
 # select an app from the selected cluster, skip promt when only one match
-service=$(echo "$apps" | cluster=$cluster yq '(.[] | select(.metadata.labels.cluster == strenv(cluster)).metadata.labels.application)' | fzf --select-1 --query="${2:-}" || true)
+service=$(
+  echo "$apps" | cluster=$cluster yq '(.[] | select(.metadata.labels.cluster == strenv(cluster)).metadata.labels.application)' | fzf --select-1 --query="${2:-}" || true
+)
 
 # get the service URL from the cluster / app combination
-url=$(echo "$apps" | cluster=$cluster service=$service yq '(.[] | select(.metadata.labels.cluster == strenv(cluster)) | select(.metadata.labels.application == strenv(service)).spec.public_addr)')
+url=$(
+  echo "$apps" | cluster=$cluster service=$service yq '(.[] | select(.metadata.labels.cluster == strenv(cluster)) | select(.metadata.labels.application == strenv(service)).spec.public_addr)'
+)
 
 # set default URL when user skips the promts
 if [ -z "${url}" ]; then
